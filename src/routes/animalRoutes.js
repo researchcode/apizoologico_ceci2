cac1d62b6e84ea6b76f695360e9ec69ff9c71ece
Ceci <ceciliaagarzon@yahoo.com>
2025-04-08 11:55:02 -0500

    API Post y Get todos

---
+++ src/routes/animalRoutes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const animalSchema = require("../models/animalModel");
+//Nuevo animal
+router.post("/animals", (req, res) => {
+    const animal = animalSchema(req.body);
+    animal
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+//Consultar todos los animales
+router.get("/animals", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Consultar un animal por su id
+router.get("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+module.exports = router;
